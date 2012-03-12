94569fdf52e79d76eff0ed50f117ff53a3987e3a
Josh Johnston <johnston.joshua@gmail.com>
2012-03-12 14:15:54 -0400

    whoops, was using an old license template in my home netbeans install that was leftover from importing my work settings into my home netbeans

--- example/application/resources/Example/IndexResource.php
+++ example/application/resources/Example/IndexResource.php
@@ -1,17 +1,45 @@
 <?php
 /**
- * Master BackOffice System (MBS)
- *
- * This file belongs to LexisNexis MBS
+ * RestPHP Framework
  *
  * PHP Version 5.3
  *
- * @author     Joshua Johnston <joshua.johnston@lexisnexis.com>
- * @copyright  2011 LexisNexis
- * @category   Example
- * @package    Example
- * @version    $Id:$
+ * Copyright (c) 2011, RestPHP Framework
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the RestPHP Framework nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   RestPHP
+ * @package    RestPHP
+ * @author     Joshua Johnston <johnston.joshua@gmail.com>
+ * @copyright  2011 RestPHP Framework
+ * @license    http://opensource.org/licenses/bsd-license.php New BSD License
  */
+
 
 /**
  * @namespace
@@ -19,9 +47,13 @@
 namespace Example;
 
 /**
+ * Example resource
  *
- * @package    Example
- * @author     jjohnston
+ * @category   RestPHP
+ * @package    RestPHP
+ * @author     Joshua Johnston <johnston.joshua@gmail.com>
+ * @copyright  2011 RestPHP Framework
+ * @license    http://opensource.org/licenses/bsd-license.php New BSD License
  */
 class IndexResource extends \RestPHP\Resource\Resource
 {
